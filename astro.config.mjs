362a459094c67b081509c646d5417dc49a5922c4
ivan <ivanvaneck2000@gmail.com>
2024-09-18 00:49:26 +0300

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thorns-1.github.io/',
+  site: 'https://thorns-1.github.io/personal-website/',
 });
